87b80dda0883ed52f0b73ae2f29f7c761410705e
解志达 <eva330680229@gmail.com>
2016-04-06 16:25:23 +0800

    修改大神的代码争取彻底去掉i18n

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,27 +3,26 @@
 from __future__ import unicode_literals
 from os import getenv
 
-AUTHOR = 'farseerfc'
-SITENAME = "Farseerfc的小窩"
+AUTHOR = 'Silver'
+SITENAME = "Silver's Blog"
 
-SITEURL = '//' + getenv("SITEURL", default='localhost:8000')
+SITEURL = 'http://123.206.29.21/'
 
-TIMEZONE = 'Asia/Tokyo'
+TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = 'zh'
-LOCALE = 'zh_HK.utf8'
+DEFAULT_LANG = 'zhs'
+LOCALE = 'zh_CN.utf8'
 
 DATE_FORMATS = {
-    'en': ((u'en_US', 'utf8'), u'%a, %d %b %Y',),
-    'zh': ((u'zh_HK', 'utf8'), u'%Y年%m月%d日(週%a)',),
     'zhs': ((u'zh_CN', 'utf8'), u'%Y年%m月%d日(周%a)',),
-    'jp': ((u'ja_JP', 'utf8'), u'%Y年%m月%d日(%a曜日)',),
 }
 
 
-DISQUS_SITENAME = 'farseerfcgithub'
+
+DISQUS_SITENAME = 'silverchard'
 DISQUS_DISPLAY_COUNTS = True
-GOOGLE_ANALYTICS = 'UA-29540705-1'
+GOOGLE_ANALYTICS = 'UA-75935913-1'
+
 
 
 TAG_FEED_ATOM = None
@@ -31,7 +30,7 @@ FEED_ATOM = None
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 
-DEFAULT_PAGINATION = 12
+DEFAULT_PAGINATION = 10
 
 STATIC_PATHS = ['static',
                 'images',
@@ -55,23 +54,13 @@ THEME = "theme"
 
 
 I18N_SUBSITES = {
-    'jp': dict(
-        LOCALE='ja_JP.utf8',
-        SITENAME="Farseerfcの巣",
-        STATIC_PATHS=STATIC_PATHS
-    ),
-    'en': dict(
-        LOCALE='en_US.utf8',
-        SITENAME="Farseerfc's Nest",
-        STATIC_PATHS=STATIC_PATHS
-    ),
     'zhs': dict(
         LOCALE='zh_CN.utf8',
-        SITENAME="Farseerfc的小窝",
+        SITENAME="Silver's Blog",
         STATIC_PATHS=STATIC_PATHS
     ),
 }
-I18N_UNTRANSLATED_ARTICLES = "remove"
+# I18N_UNTRANSLATED_ARTICLES = "remove"
 
 MD_EXTENSIONS = ['admonition',
                  'toc',
@@ -80,14 +69,10 @@ MD_EXTENSIONS = ['admonition',
 
 PLUGINS = ["i18n_subsites",
            "better_codeblock_line_numbering",
-           "plantuml",
-           "youku",
-           "youtube",
            'tipue_search',
            'neighbors',
            'series',
            'bootstrapify',
-           'twitter_bootstrap_rst_directives',
            "render_math",
            'extract_toc',
            'tag_cloud',
@@ -108,44 +93,39 @@ CACHE_CONTENT = True
 
 # Theme options
 
-JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+# JINJA_EXTENSIONS = ['jinja2.ext.i18n']
 
 DOCUTIL_CSS = True
 TYPOGRIFY = False
 PYGMENTS_STYLE = 'monokai'
-GITHUB_USER = 'farseerfc'
+GITHUB_USER = 'SilverChard'
 GITHUB_SHOW_USER_LINK = True
 DISPLAY_BREADCRUMBS = True
 DISPLAY_CATEGORY_IN_BREADCRUMBS = True
 CC_LICENSE = "CC-BY-NC-SA"
 DISPLAY_TAGS_INLINE = True
 OUTPUT_SOURCES = True
-OUTPUT_SOURCES_EXTENSION = '.rst'
+OUTPUT_SOURCES_EXTENSION = '.md'
 
-# WEIBO_WIDGET = True
-# WEIBO_APPKEY = "NANnN"
-# WEIBO_APPKEYN = "498769639"
 
 DIRECT_TEMPLATES = (('search', 'index', 'categories', 'authors', 'archives',
                      'tags'))
 
-# TWITTER_USERNAME = 'farseerfc'
-# TWITTER_WIDGET_ID = "538997172142759936"
 
 AVATAR = 'images/avatar.jpg'
 ABOUT_PAGE = "about.html"
 ABOUT_ME = """<h3 style="text-align:center">
-<a href="https://twitter.com/farseerfc"                  target="_blank">
+<a href="https://twitter.com/Silver_Chard"                  target="_blank">
 <i class="fa fa-twitter" style="text-align:center"></i></a>
-<a href="https://github.com/farseerfc"                   target="_blank">
+<a href="https://github.com/SilverChard"                   target="_blank">
 <i class="fa fa-github" style="text-align:center"></i></a>
-<a href="http://weibo.com/farseerfc"                     target="_blank">
+<a href="http://weibo.com/1843103547"                     target="_blank">
 <i class="fa fa-weibo" style="text-align:center"></i></a>
-<a href="http://www.facebook.com/farseerfc"              target="_blank">
+<a href="https://www.facebook.com/ChardShieh"              target="_blank">
 <i class="fa fa-facebook" style="text-align:center"></i></a>
-<a href="https://plus.google.com/u/0/+JiachenYang/posts" target="_blank">
+<a href="https://plus.google.com/u/0/+SilverChard" target="_blank">
 <i class="fa fa-google-plus" style="text-align:center"></i></a>
-<a href="mailto:farseerfc@gmail.com" target="_blank">
+<a href="mailto:eva330680229@gmail.com" target="_blank">
 <i class="mdi-communication-email" style="text-align:center"></i></a>
 </h3>
 """
